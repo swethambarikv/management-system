1609a9954a573a1f9669a2e5056f9596d2db5631
swethambarikv <93245352+swethambarikv@users.noreply.github.com>
2022-05-11 19:23:18 +0530

    Update user.component.ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
+import { LoginService } from './../service/login.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, NgForm,ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 import { User1} from './user';
 
 @Component({
@@ -11,14 +13,34 @@ export class UserComponent implements OnInit {
 
   
   userLogin=new User1('Swethambari','Swetha27@');
-
-  constructor(private router:Router){}
-
-  btnClick(){
-    this.router.navigate(['/login']);
-  }
+  roleList: any = ['Admin','User'];
+  username: string = '';
+  psw: string = '';
+  role:string='';
+  // userform: FormGroup;
 
   ngOnInit(): void {
   }
 
+  constructor(private router: Router,private loginService:LoginService){}
+
+  btnClick(){
+    this.router.navigate(['/login']);
+  }
+  display(formData: NgForm) {
+    console.log(formData.value);
+
+    if (this.role == "admin") {
+      console.warn("you are admin");
+
+      this.router.navigate(['/login']);
+    }
+    else {
+ 
+      console.warn("you are user");
+
+      this.router.navigate(['/table']);
+    }
+  }
 }
+
