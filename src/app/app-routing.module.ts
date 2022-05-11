2768fe700e7c684d4b2c49b2b9d0333866b9dbc9
swethambarikv <93245352+swethambarikv@users.noreply.github.com>
2022-05-11 19:23:41 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { DispalyComponent } from './dispaly/dispaly.component';
+import { DispalyComponent } from './display/dispaly.component';
 import { LoginComponent } from './login/login.component';
 import { TableComponent } from './table/table.component';
-
 import { UserComponent } from './user/user.component';
 const routes: Routes = [
 {path:'login' ,component:LoginComponent,},
 { path: 'user', component: UserComponent},
-{ path: 'table', component: TableComponent,
-children:[{
-path: 'dispaly/:id', component: DispalyComponent}
-]}
+{ path: 'table', component: TableComponent},
+{path: 'update-add/:id', component: LoginComponent}
 ];
 
 @NgModule({
