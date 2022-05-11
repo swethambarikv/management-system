d3cc00e161f9dcd8a5fc867dc176c2783b4aca70
swethambarikv <93245352+swethambarikv@users.noreply.github.com>
2022-05-11 19:18:14 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,19 +12,36 @@ export class LoginComponent {
   title = 'tdfForm';
   topics =['angular','react','php'];
   userModel = new User();
-  constructor(private router:Router,private login:LoginService){}
+  id: number = 0;
+  constructor(private router:Router,private login:LoginService,private route: ActivatedRoute){}
   ngOnInit(){
     let data = localStorage.getItem(this.userModel.name);
     console.log(" ans "+this.userModel.name);
     console.log(" data :"+data);
+    console.log("Id : "+this.userModel.id);
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      if (this.id != null) {
+        this.userModel.id=(params['id']);
+        const data = this.login.getUsersByID(this.id);
+        if (data) {
+          this.userModel=(data);
+        }
+      }
+    });
   }
   userdata()
   {
-    this.router.navigate(['/table']);
-    this.login.setMessage(this.userModel);
-    localStorage.setItem(this.userModel.name, JSON.stringify(this.userModel));
-    // this.login.userName(this.userModel);
-    // this.login.display(this.userModel);
+    // this.router.navigate(['/table']);
+    // this.login.setMessage(this.userModel);
+    // localStorage.setItem(this.userModel.name, JSON.stringify(this.userModel));
+    if (this.userModel.id === 0) {
+      //Create New User
+      console.log("id : "+this.userModel.id)
+      this.login.setMessage(this.userModel);
+    } else {
+      //Update User info
+      this.login.updateUser(this.userModel);
   }
-  
-}
+  this.router.navigate(['/table']);
+  }}
