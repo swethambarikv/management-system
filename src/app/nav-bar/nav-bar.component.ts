9e4726f8390f2ea46e274e755ce41bd74d80a6d2
swethambarikv <93245352+swethambarikv@users.noreply.github.com>
2022-05-11 19:19:12 +0530

    Update nav-bar.component.ts

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,25 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoginService } from '../service/login.service';
 import { User } from '../user1';
+
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
-export class NavBarComponent implements OnInit {
-  userList: User[] = [];
-  display :boolean = false;
-  constructor(private login:LoginService,private router:Router) { }
-
-  ngOnInit(): void {
-    this.userList=this.login.getUsers();
+export class LoginComponent {
+  title = 'tdfForm';
+  topics =['angular','react','php'];
+  userModel = new User();
+  id: number = 0;
+  constructor(private router:Router,private login:LoginService,private route: ActivatedRoute){}
+  ngOnInit(){
+    let data = localStorage.getItem(this.userModel.name);
+    console.log(" ans "+this.userModel.name);
+    console.log(" data :"+data);
+    console.log("Id : "+this.userModel.id);
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      if (this.id != null) {
+        this.userModel.id=(params['id']);
+        const data = this.login.getUsersByID(this.id);
+        if (data) {
+          this.userModel=(data);
+        }
+      }
+    });
   }
-
-  logIn(){
-    this.display=true;
+  userdata()
+  {
+    // this.router.navigate(['/table']);
+    // this.login.setMessage(this.userModel);
+    // localStorage.setItem(this.userModel.name, JSON.stringify(this.userModel));
+    if (this.userModel.id === 0) {
+      //Create New User
+      console.log("id : "+this.userModel.id)
+      this.login.setMessage(this.userModel);
+    } else {
+      //Update User info
+      this.login.updateUser(this.userModel);
   }
-  logOut(){
-    this.display=false;
-  }
-}
+  this.router.navigate(['/table']);
+  }}
